9078432a199c96fc002bc90090e6acfdc7e0441a
Anthony Honstain <honstain@gmail.com>
2014-07-06 00:20:06 -0700

    incremental checking

--- src/main/webapp/javascript/chapter4functions.js
+++ src/main/webapp/javascript/chapter4functions.js
@@ -5,3 +5,17 @@
 /**
  * Scope - Page 36
  */
+
+var foo = function () {
+    var a = 3, b = 5;
+
+    var bar = function () {
+        var b = 7, c = 11;
+
+        a += b + c;
+    };
+
+    bar();
+};
+
+document.writeln("" + a + " " + b);
